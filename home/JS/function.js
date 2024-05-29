0455c04d50cef8ea2992cef8c266755cd55f6381
youna99 <dbsk0108@gmail.com>
2024-05-29 22:18:53 +0900

    study: JS 함수, 조건문 복습

---
+++ home/JS/function.js
@@ -0,0 +1,15 @@
+function multifly(x, y) {
+    return x * y;
+};
+
+console.log(multifly(3, 7));
+console.log(multifly(2,2));
+
+
+function square(a) {
+    console.log(a**2);
+};
+
+square(4);
+square(11);
+square(5);
---
+++ home/JS/if.js
@@ -0,0 +1,15 @@
+let age = prompt('나이를 입력해주세요~')
+
+if (age >= 20){
+    console.log('성인');
+} else if (age >= 17) {
+    console.log('고등학생');
+} else if (age >= 14) {
+    console.log('중학생');
+} else if (age >= 8) {
+    console.log('초등학생');
+} else if (age >= 0) {
+    console.log('유아');
+} else {
+    console.log('잘못 입력하셨습니다!');
+}
