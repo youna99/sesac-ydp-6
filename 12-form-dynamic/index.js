23529ba2021c3ebde8250ccebd8d8b95a2eee6e3
youna99 <dbsk0108@gmail.com>
2024-06-17 17:26:49 +0900

    study: axios, fetch

--- 12-form-dynamic/index.js
+++ 12-form-dynamic/index.js
@@ -1,30 +1,51 @@
-const express = require("express");
+const express = require('express');
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs");
-app.set("views", "./views");
-
-app.use(express.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
+app.set('views', './views');
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.get('/', (req, res) => {
-    res.render('dynamic', {title: '동적 폼을 배워보자!'})
-});
+    res.render('dynamic', { title: '동적 폼을 배워보자!' })
+})
 
 // ajax
 app.get('/ajax', (req, res) => {
     // GET 방식이므로 브라우저에서 URL 쿼리까지 직접 입력해서도 값을 확인 가능
-    // ex. http://localhost:8080/ajax?name=df&gender=%EB%82%A8%EC%9E%90
-    console.log(req.query);  // { name: 'df', gender: '남자' }
+    // ex. http://localhost:8080/ajax?name=션&gender=남자
+    console.log(req.query);
     res.send(req.query);
-});
+})
 
 app.post('/ajax', (req, res) => {
     console.log(req.body);
     res.send(req.body);
-});
+})
+
+// axios
+app.get('/axios', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+})
+
+app.post('/axios', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
+
+// fetch
+app.get('/fetch', (req, res) => {
+    console.log(req.query);
+    res.send(req.query);
+})
+
+app.post('/fetch', (req, res) => {
+    console.log(req.body);
+    res.send(req.body);
+})
 
 app.listen(PORT, () => {
     console.log(`http://localhost:${PORT}`);
-});
+})
