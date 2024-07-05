451303f86d810cc3e191e4ea63ea614345a0adbe
youna99 <dbsk0108@gmail.com>
2024-07-05 16:51:25 +0900

    study: 웹소켓

--- 21-socket.io/server.js
+++ 21-socket.io/server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const http = require('http');
+const { send } = require('process');
 // node.js 기본 내장 모듈인 'http' 불러오기.
 // 'http' 모듈은 HTTP 서버와 클라이언트 기능을 제공.
 
@@ -26,6 +27,12 @@ app.get('/', (req, res) => {
 // 사용자 닉네임 모음 객체
 const nickObjs = {};
 
+// [실습 3-2-3]
+// 유저 목록 업데이트
+function updateList() {
+    io.emit('updateNicks', nickObjs) // 전체 사용자 닉네임 모음 객체 전달
+}
+
 // io.on() : socket 관련한 통신 작업을 처리
 io.on('connection', (socket) => {
     // connection 이벤트는 클라이언트가 접속 했을 때 발생
@@ -41,7 +48,7 @@ io.on('connection', (socket) => {
     // })
 
     // [실습 3] 채팅창 입장 안내 문구
-    io.emit('notice', `${socket.id} 님이 입장하셨습니다.`);
+    // io.emit('notice', `${socket.id} 님이 입장하셨습니다.`);
 
     // [실습 3-2] 채팅창 입장 문구 socket.id -> nickname
     // emit() from server
@@ -54,6 +61,63 @@ io.on('connection', (socket) => {
 
         // [실습 3-2-1]
         // 프론트에서 입력한 nick이 nickObjs 객체에 존재하는지 검사.
+        // 이미 존재 : error 이벤트 + '이미 존재하는 닉네임 입니다'  
+        // => 클라이언트 : error 이벤트 받으면 alert 띄우기.
+        // 새 닉네임 : notice 이벤트 + ${nick} 님이 입장하셨습니다.
+        if (Object.values(nickObjs).indexOf(nick) > - 1) {
+            // 이미 존재하는 닉네임 있음.
+            socket.emit('error', '이미 존재하는 닉네임 입니다.');
+        } else {
+            // 새로운 닉네임
+            nickObjs[socket.id] = nick;
+            console.log('접속 유저 목록 :: ', nickObjs);
+            io.emit('notice', `${nick} 님이 입장하셨습니다.`); // 전체 공지
+
+            // [실습 3-2-2]
+            socket.emit('entrySuccess', nick); // 해당 소켓 데이터를 전송.
+            updateList();
+        }
+    })
+
+    // [실습 3-3] 클라이언트 퇴장시
+    // "notice" 이벤트로 퇴장 공지
+    socket.on('disconnect', () => {
+        console.log("접속 끊김 :: ", `${nickObjs[socket.id]} 님 퇴장 ::` , socket.id);
+
+        io.emit('notice', `${nickObjs[socket.id]} 님이 퇴장하셨습니다.`);
+        delete nickObjs[socket.id]; // 닉네임 삭제
+        updateList();
+    })
+
+    // [실습 4] 채팅창 메시지 전송
+    // send 이벤트를 받아서
+    // 모두에게 newMessage 이벤트로 {닉네임, 입력창 내용} 데이터를 전송.
+    socket.on('send', (data) => {
+        // { dm: '?', myNick: 'A', msg: 'B' } 형식
+        // console.log("서버 측 - data :: ", data);
+        // "전체 발송"
+        // io.emit('newMessage', {nick : data.myNick, msg: data.msg }) -- DM 기능, IF문 시 주석.
+
+        // [실습 5] DM 기능 추가하기
+        // DM 인지 아닌지 구분해서
+        // io.to(소켓아이디).emit(event_name, data) : 소켓아이디에 해당하는 클라이언트에게'만' 전송.
+
+        if(data.dm === 'all') {
+            // "전체" 발송
+            io.emit('newMessage', {nick : data.myNick, msg: data.msg })
+        } else {
+            // "DM" 발송
+            let dmSocketId = data.dm;
+            const sendData = {
+                nick: data.myNick,
+                msg: data.msg,
+                dm:'(속닥속닥)',
+            }
+
+            io.to(dmSocketId).emit('newMessage', sendData) // DM을 보내야하는 타겟(소켓아이디)한테 메세지 전송.
+            socket.emit('newMessage', sendData);
+            // console.log("sendData >>> ", sendData ); { nick: '파이리', msg: '안녕', dm: '(속닥속닥)' }
+        } 
     })
 })
 
