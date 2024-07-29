1bf266d5eb0ec0798344b374fb4b9a5af70e772f
youna <dbsk0108@gmail.com>
2024-07-29 23:55:08 +0900

    study: props 복습

--- 24-props/src/App.js
+++ 24-props/src/App.js
@@ -1,20 +1,20 @@
-import './App.css';
-import Button from './Button';
-import ClassComponent from './ClassComponent';
-import FunctionComponent from './FunctionComponent';
+import "./App.css";
+import Button from "./Button";
+import ClassComponent from "./ClassComponent";
+import FunctionComponent from "./FunctionComponent";
 
 function App() {
   return (
     <div className="App">
-        <ClassComponent />
-        <ClassComponent name="SeSAC 6기"/>
-        <hr />
+      <ClassComponent />
+      <ClassComponent name="SeSAC 6기" />
+      <hr />
 
-        <FunctionComponent />
-        <FunctionComponent name="SeSAC 6기"/>
-        <hr />
+      <FunctionComponent />
+      <FunctionComponent name="SeSAC 6기" />
+      <hr />
 
-        <Button link="https://www.google.com">Google</Button>
+      <Button link="https://www.google.com">Google</Button>
     </div>
   );
 }
