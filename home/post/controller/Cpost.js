323296fc0037cb74e2d6b7ff5c97bfee3acec1a5
youna <dbsk0108@gmail.com>
2024-07-31 00:01:23 +0900

    게시판 수정 삭제 복습 완료

--- home/post/controller/Cpost.js
+++ home/post/controller/Cpost.js
@@ -1,3 +1,4 @@
+const { where } = require("sequelize");
 const { Post } = require("../models/index");
 
 // 글 목록
@@ -11,6 +12,7 @@ exports.main = async (req, res) => {
   }
 };
 
+// 등록
 exports.postPost = async (req, res) => {
   try {
     console.log(req.body);
@@ -20,6 +22,61 @@ exports.postPost = async (req, res) => {
       content,
       writer,
     });
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 삭제
+exports.deletePost = async (req, res) => {
+  try {
+    console.log(req.body);
+
+    const { id } = req.body;
+    const isDeleted = await Post.destroy({
+      where: { id },
+    });
+    console.log("isDeleted", isDeleted);
+
+    if (isDeleted) {
+      return res.send(true);
+    } else {
+      return res.send(false);
+    }
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 하나 조회
+exports.getPost = async (req, res) => {
+  try {
+    console.log(req.params);
+    const { id } = req.params;
+
+    const post = await Post.findOne({
+      where: { id },
+    });
+    res.json(post);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+// 수정
+exports.patchPost = async (req, res) => {
+  try {
+    console.log(req.params);
+    console.log(req.body);
+
+    const { id } = req.params;
+    const { title, content, writer } = req.body;
+
+    const post = await Post.update(
+      { title, content, writer },
+      { where: { id } }
+    );
     res.json(post);
   } catch (error) {
     console.error(error);
