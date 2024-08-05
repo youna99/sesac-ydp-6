563a73bbce88103dc247fbc21935cca9aa48a834
youna99 <dbsk0108@gmail.com>
2024-08-05 14:09:24 +0900

    파일 정리

--- encryption/middleware/routerMiddleware.js
+++
@@ -1,9 +0,0 @@
-const mainRouter = require('../router/mainRouter');
-const memberRouter = require('../router/memberRouter');
-const boardRouter = require('../router/boardRouter');
-
-exports.routerMiddleware = (app) => {
-  app.use('/', mainRouter);
-  app.use('/board', boardRouter);
-  app.use('/member', memberRouter);
-};
--- encryption/workbench.sql
+++
@@ -1,8 +0,0 @@
-use codingon;
-
-show tables;
-
-insert into member values(null,'프로젝트33','1234',true,now(),now(),null);
-
-select * from board;
-select * from member;
