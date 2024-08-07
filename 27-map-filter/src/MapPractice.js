eb50d41e53c240a0eb2c1cb64ce62152bad95766
youna <dbsk0108@gmail.com>
2024-08-08 00:40:04 +0900

    study: map, filter 코드 수정

--- 27-map-filter/src/MapPractice.js
+++ 27-map-filter/src/MapPractice.js
@@ -1,45 +1,72 @@
-import React, { useState } from 'react'
+import React, { useState } from "react";
 
 function MapPractice() {
+  const [text, setText] = useState([
+    {
+      id: 1,
+      name: "코디",
+      email: "codi@gmail.com",
+    },
+    {
+      id: 2,
+      name: "윤소희",
+      email: "yoonsohee@gmail.com",
+    },
+  ]);
 
-    const [text, setText] = useState([
-        {
-            id: 1,
-            name: '코디',
-            email: 'codi@gmail.com',
-        },
-        {
-            id: 2,
-            name: '윤소희',
-            email: 'yoonsohee@gmail.com',
-        },
-    ]);
+  const [inputName, setInputName] = useState("");
+  const [inputEmail, setInputEmail] = useState("");
 
-    const [inputName, setInputName] = useState('');
-    const [inputEmail, setInputEmail] = useState('');
+  const addText = () => {
+    const newText = text.concat({
+      id: text.length + 1,
+      name: inputName,
+      email: inputEmail,
+    });
+    setText(newText);
+    setInputName("");
+    setInputEmail("");
+  };
 
-    const addText = () => {
-        const newText = text.concat({
-            id: text.length + 1,
-            name: inputName,
-            email: inputEmail,
-        });
-        setText(newText);
-        setInputName('');
-        setInputEmail('')
+  const handleKeyDown = (e) => {
+    // console.log("e >>", e);
+    if (e.keyCode === 13) {
+      addText();
     }
+  };
+
+  const deleteText = (valueID) => {
+    const newText = text.filter((text) => text.id !== valueID);
+
+    setText(newText);
+  };
   return (
     <div>
-        <input type="text" placeholder='이름' value={inputName} onChange={(e) => {
-            setInputName(e.target.value);
-        }}/>
-        <input type="email" placeholder='이메일' value={inputEmail} onChange={(e) => {setInputEmail(e.target.value)}}/>
-        <button onClick={addText}>등록</button>
-        {text.map((value) => (
-            <h2 key={value.id}>{value.name} : {value.email}</h2>
-        ))}
+      <input
+        type="text"
+        placeholder="이름"
+        value={inputName}
+        onChange={(e) => {
+          setInputName(e.target.value);
+        }}
+      />
+      <input
+        type="email"
+        placeholder="이메일"
+        value={inputEmail}
+        onChange={(e) => {
+          setInputEmail(e.target.value);
+        }}
+        onKeyDown={(e) => handleKeyDown(e)}
+      />
+      <button onClick={addText}>등록</button>
+      {text.map((value) => (
+        <h2 key={value.id} onDoubleClick={() => deleteText(value.id)}>
+          {value.name} : {value.email}
+        </h2>
+      ))}
     </div>
-  )
+  );
 }
 
-export default MapPractice
+export default MapPractice;
