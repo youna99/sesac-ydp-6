e164d806b7812caca279663ad9754a2752bea930
youna <dbsk0108@gmail.com>
2024-08-13 10:33:36 +0900

    Sass 실습 (시계방향으로 움직이는 공)

--- 30-styles/src/SassPrac2.js
+++ 30-styles/src/SassPrac2.js
@@ -1,11 +1,12 @@
-import React from 'react'
+import React from "react";
+import "./styles/SassPrac2.scss";
 
 export default function SassPrac2() {
   return (
-    <div className='container'>
-        <div className='c1'></div>
-        <div className='c2'></div>
-        <div className='c3'></div>
+    <div className="container">
+      <div className="circle c1"></div>
+      <div className="circle c2"></div>
+      <div className="circle c3"></div>
     </div>
-  )
+  );
 }
